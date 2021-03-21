758a3b25eb26b84e0f4a13148427d07feb0b2c4f
JorgeGarcia20 <alberto-garcia20@outlook.com>
2021-03-21 12:51:47 -0600

    Sobrescritura de miembros

---
+++ src/sobrescrituraDeMiembros/Animal.java
@@ -0,0 +1,14 @@
+package sobrescrituraDeMiembros;
+
+public class Animal {
+    /*
+    * La sobrescritura de miembros se observa al cambiar un metodo proveniente de una clase
+    * padre.
+    * a comparacion de la sobrecarga que se realiza solo en la clase, la sobrescritura de miembros
+    * se hace en clases que heredean de la clase padre.
+    * */
+
+    public void comer(){
+        System.out.println("Estoy comiendo....");
+    }
+}
---
+++ src/sobrescrituraDeMiembros/Vaca.java
@@ -0,0 +1,8 @@
+package sobrescrituraDeMiembros;
+
+public class Vaca extends Animal {
+    @Override
+    public void comer() {
+        System.out.println("Estoy comiendo pasto.......Muuuu!");
+    }
+}
